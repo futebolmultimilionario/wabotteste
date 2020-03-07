e921b4a24f62debb311722a719176b886e570ca0
albertogomesf <55947857+albertogomesf@users.noreply.github.com>
2020-03-07 16:26:34 -0300

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
         $token = 'oo80b8hpghlw5y58';
 
         // $update = file_get_contents("php://input");
-	$update = file_get_contents($APIurl."messages?token=".$token."&chatId=5511948010386-1552934954@g.us&last");
+	$update = file_get_contents($APIurl."messages?token=".$token."&last");
 
         $updateArray = json_decode($update, TRUE);
 	
