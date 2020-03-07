c851e89f6a30bfc7adacf906f1ee01847c07bc40
albertogomesf <55947857+albertogomesf@users.noreply.github.com>
2020-03-07 14:53:31 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+﻿<?php
+        $APIurl = 'https://eu2.chat-api.com/instance104918/';
+        $token = 'oo80b8hpghlw5y58';
+
+        $update = file_get_contents("php://input");
+
+        $updateArray = json_decode($update, TRUE);
+
+        $texten = $updateArray["messages"][0]["body"];
+
+        $text = str_replace(' ', '%20', $texten);
+
+        file_get_contents($APIurl."sendMessage?token=".$token."&chatId=558398858522-1568030251@g.us&body=".$text)
+
+?>
