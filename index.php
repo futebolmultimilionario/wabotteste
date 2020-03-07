c395a9206c02416233ac428aeb87cab8f99e4a03
albertogomesf <55947857+albertogomesf@users.noreply.github.com>
2020-03-07 16:25:01 -0300

    Update index.php

--- index.php
+++ index.php
@@ -7,10 +7,12 @@
 
         $updateArray = json_decode($update, TRUE);
 	
-        $texten = $updateArray["messages"][99]["body"];
-
-        $text = urlencode($texten);
-
-        file_get_contents($APIurl."sendMessage?token=".$token."&chatId=558398858522-1568030251@g.us&body=".$text)
-
+	if ($updateArray["messages"][99]["chatId"] == "5511948010386-1552934954@g.us") {
+        	$texten = $updateArray["messages"][99]["body"];
+        	$text = urlencode($texten);
+        	file_get_contents($APIurl."sendMessage?token=".$token."&chatId=558398858522-1568030251@g.us&body=".$text);
+	} else {
+		echo "NoCommand";
+	}
+			
 ?>
