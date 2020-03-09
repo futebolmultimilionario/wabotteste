c743cd797633c915695dd64737cd95217f644f35
albertogomesf <55947857+albertogomesf@users.noreply.github.com>
2020-03-08 21:43:56 -0300

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 
         $updateArray = json_decode($update, TRUE);
 	
-	if ($updateArray["messages"][0]["chatId"] == "5511948010386-1552934954@g.us") {
+	if ($updateArray["messages"][0]["chatId"] == "558399711150-1558430352@g.us") {
         	$texten = $updateArray["messages"][0]["body"];
         	$text = urlencode($texten);
         	file_get_contents($APIurl."sendMessage?token=".$token."&chatId=558398858522-1568030251@g.us&body=".$text);
