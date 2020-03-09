ab6b34f6018886dcae1b4b9349b736b7c158ef39
albertogomesf <55947857+albertogomesf@users.noreply.github.com>
2020-03-08 21:49:35 -0300

    Update index.php

--- index.php
+++ index.php
@@ -8,12 +8,18 @@
         $updateArray = json_decode($update, TRUE);
 	
 	if ($updateArray["messages"][0]["chatId"] == "558399711150-1558430352@g.us") {
-        	$texten = $updateArray["messages"][0]["body"];
-        	$text = urlencode($texten);
-        	file_get_contents($APIurl."sendMessage?token=".$token."&chatId=558398858522-1568030251@g.us&body=".$text);
-	} else {
-		echo "NoCommand";
-	}
+          if($updateArray["messages"][0]["type"] == "chat") {
+            $texten = $updateArray["messages"][0]["body"];
+        	  $text = urlencode($texten);
+        	  file_get_contents($APIurl."sendMessage?token=".$token."&chatId=558398858522-1568030251@g.us&body=".$text);
+          } if ($updateArray["messages"][0]["type"] == "image") {
+            $texten = $updateArray["messages"][0]["body"];
+            $text = urlencode($texten);
+            file_get_contents($APIurl."sendFile?token=".$token."&chatId=558398858522-1568030251@g.us&body=".$text."&filename=1554d15f125d.jpg");
+        }
+        } else {
+		      echo "NoCommand";
+	      }
 			
 ?>
 
