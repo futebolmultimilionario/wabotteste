7f0958d1996df29f32797aafa65d62df6e8a9455
albertogomesf <55947857+albertogomesf@users.noreply.github.com>
2020-03-08 21:06:45 -0300

    Update index.php

--- index.php
+++ index.php
@@ -3,16 +3,22 @@
         $token = 'oo80b8hpghlw5y58';
 
         $update = file_get_contents("php://input");
-	// $update = file_get_contents($APIurl."messages?token=".$token."&last");
-
+	      
         $updateArray = json_decode($update, TRUE);
-	
+
+
 	if ($updateArray["messages"][0]["chatId"] == "5511948010386-1552934954@g.us") {
-        	$texten = $updateArray["messages"][0]["body"];
-        	$text = urlencode($texten);
-        	file_get_contents($APIurl."sendMessage?token=".$token."&chatId=558398858522-1568030251@g.us&body=".$text);
-	} else {
-		echo "NoCommand";
+          if($updateArray["messages"][0]["type"] == "chat") {
+            $texten = $updateArray["messages"][0]["body"];
+            $text = urlencode($texten);
+            file_get_contents($APIurl."sendMessage?token=".$token."&chatId=558398858522-1568030251@g.us&body=".$text);
+        } if ($updateArray["messages"][0]["type"] == "image") {
+            $texten = $updateArray["messages"][0]["body"];
+            $text = urlencode($texten);
+            file_get_contents($APIurl."sendFile?token=".$token."&chatId=558398858522-1568030251@g.us&body=".$text."&filename=1554d15f125d.jpg");
+        }
+        } else {
+	    echo "NoCommand";
 	}
-			
+
 ?>
