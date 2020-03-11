4aa584afe150e18b7bdcd95f5f1e17c44e145c9c
albertogomesf <55947857+albertogomesf@users.noreply.github.com>
2020-03-10 22:50:49 -0300

    Update index.php

--- index.php
+++ index.php
@@ -6,24 +6,22 @@
 	// $update = file_get_contents($APIurl."messages?token=".$token."&last");
 
         $updateArray = json_decode($update, TRUE);
-	
-	if ($updateArray["messages"][0]["chatId"] == "5511948010386-1552934954@g.us") {
+        $texten = $updateArray["messages"][0]["body"];
+        $text = urlencode($texten);
+
+        if ($updateArray["messages"][0]["chatId"] == "5511948010386-1552934954@g.us") {
           if($updateArray["messages"][0]["type"] == "chat") {
-            $texten = $updateArray["messages"][0]["body"];
-        	  $text = urlencode($texten);
-        	  file_get_contents($APIurl."sendMessage?token=".$token."&chatId=558398858522-1568030251@g.us&body=".$text);
+            if(strlen($updateArray["messages"][0]["body"])<601){
+        	    file_get_contents($APIurl."sendMessage?token=".$token."&chatId=558398858522-1568030251@g.us&body=".$text);
+            }
           } if ($updateArray["messages"][0]["type"] == "image") {
-            $texten = $updateArray["messages"][0]["body"];
-            $text = urlencode($texten);
             file_get_contents($APIurl."sendFile?token=".$token."&chatId=558398858522-1568030251@g.us&body=".$text."&filename=1554d15f125d.jpg");
         } } if ($updateArray["messages"][0]["chatId"] == "5511948010386-1547252688@g.us") {
           if($updateArray["messages"][0]["type"] == "chat") {
-            $texten = $updateArray["messages"][0]["body"];
-        	  $text = urlencode($texten);
-        	  file_get_contents($APIurl."sendMessage?token=".$token."&chatId=558399711150-1583678381@g.us&body=".$text);
+              if(strlen($updateArray["messages"][0]["body"])<601){
+        	      file_get_contents($APIurl."sendMessage?token=".$token."&chatId=558399711150-1583678381@g.us&body=".$text);
+          }
           } if ($updateArray["messages"][0]["type"] == "image") {
-            $texten = $updateArray["messages"][0]["body"];
-            $text = urlencode($texten);
             file_get_contents($APIurl."sendFile?token=".$token."&chatId=558399711150-1583678381@g.us&body=".$text."&filename=1554d15f125d.jpg");
         }
         } else {
