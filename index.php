0ea7e1b13f5f5e7df635cad864d19c51d43742d2
albertogomesf <55947857+albertogomesf@users.noreply.github.com>
2020-03-11 23:54:53 -0300

    Update index.php

--- index.php
+++ index.php
@@ -6,21 +6,22 @@
 	// $update = file_get_contents($APIurl."messages?token=".$token."&last");
 
         $updateArray = json_decode($update, TRUE);
-        $texten = $updateArray["messages"][0]["body"];
-        $text = urlencode($texten);
+	$text = urlencode($updateArray);
+        //$texten = $updateArray["messages"][0]["body"];
+        //$text = urlencode($texten);
         $chatIdPreJP = "558399711150-1583892427@g.us";
-        $chatIdLiveJP = "558399711150-1583892510@g.us";
-        $chatIdPreCG = "558398858522-1568030251@g.us";
-        $chatIdLiveCG = "558399711150-1583678381@g.us";
+        //$chatIdLiveJP = "558399711150-1583892510@g.us";
+        //$chatIdPreCG = "558398858522-1568030251@g.us";
+        //$chatIdLiveCG = "558399711150-1583678381@g.us";
         $chatIdPreRegys = "558399711150@c.us";
-        $chatIdLiveRegys = "5511948010386-1547252688@g.us";
-
+        //$chatIdLiveRegys = "5511948010386-1547252688@g.us";
+	
         if ($updateArray["messages"][0]["chatId"] == $chatIdPreRegys) {
         	// file_get_contents($APIurl."forwardMessage?token=".$token."&chatId=".$chatIdPreJP."&messageId=".$updateArray["messages"][0]["id"]);
-        	if($updateArray["messages"][0]["type"] == "chat") {
-        		if(strlen($texten)<601){
+        	//if($updateArray["messages"][0]["type"] == "chat") {
+        		//if(strlen($texten)<601){
         			file_get_contents($APIurl."sendMessage?token=".$token."&chatId=".$chatIdPreJP."&body=".$text);
-        		}
+        		/*}
         	}
 		if ($updateArray["messages"][0]["type"] == "image") {
         		file_get_contents($APIurl."sendFile?token=".$token."&chatId=".$chatIdPreCG."&body=".$text."&filename=1554d15f125d.jpg");
@@ -35,7 +36,7 @@
         	}
 		if ($updateArray["messages"][0]["type"] == "image") {
         		file_get_contents($APIurl."sendFile?token=".$token."&chatId=".$chatIdLiveCG."&body=".$text."&filename=1554d15f125d.jpg");
-      		}
+      		}*/
 	}
 	else {
 		echo "NoCommand";
