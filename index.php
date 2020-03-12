a9478d9c377a1ad5f3c0cbff16355320ab3cd477
albertogomesf <55947857+albertogomesf@users.noreply.github.com>
2020-03-11 23:23:11 -0300

    Update index.php

--- index.php
+++ index.php
@@ -12,8 +12,8 @@
         $chatIdLiveJP = "558399711150-1583892510@g.us";
         $chatdIdPreCG = "558398858522-1568030251@g.us";
         $chatIdLiveCG = "558399711150-1583678381@g.us";
-        $chatIdPreRegys = "5511948010386-1552934954@g.us";
-        $chatIdLiveRegys = "558399711150@c.us";
+        $chatIdPreRegys = "558399711150@c.us";
+        $chatIdLiveRegys = "5511948010386-1547252688@g.us";
 
         if ($updateArray["messages"][0]["chatId"] == $chatIdPreRegys) {
           file_get_contents($APIurl."forwardMessage?token=".$token."&chatId=".$chatIdPreJP."&messageId=".$updateArray["messages"][0]["id"]);
