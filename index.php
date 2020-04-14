416ab65536eb01e96c694a948f2034796a7c0fea
albertogomesf <55947857+albertogomesf@users.noreply.github.com>
2020-04-13 23:23:21 -0300

    Update index.php

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
         $chatIdLiveJP = "558399711150-1583892510@g.us";
         $chatIdPreCG = "558398858522-1568030251@g.us";
         $chatIdLiveCG = "558399711150-1583678381@g.us";
-        $chatIdGalgosUKJP = "558399711150@c.us";
+        $chatIdGalgosUKJP = "558399711150-1583892552@g.us";
         $chatIdGalgosUSAJP = "558399711150-1583854681@g.us";
 	$chatIdDiretoria = "558399711150-1583810992@g.us";
         $chatIdPreRegys = "5511948010386-1552934954@g.us";
