140f8b52ccd857f3bcaf5e50afae70d9feb47c11
albertogomesf <55947857+albertogomesf@users.noreply.github.com>
2020-07-12 22:32:38 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
-        $APIurl = 'https://eu127.chat-api.com/instance148694/';
-        $token = '31h67nvw3zyn6fsq';
-	$APIurl2 = 'https://eu127.chat-api.com/instance148692/';
-        $token2 = 'nlyt1a99wvn8zhpr';
+        $APIurl = 'https://eu134.chat-api.com/instance149182/';
+        $token = 'ezcota4uwraxwoeu';
+	$APIurl2 = 'https://eu134.chat-api.com/instance149181/';
+        $token2 = 'jgmd6ztbtv831j3x';
 
         $update = file_get_contents("php://input");
 
