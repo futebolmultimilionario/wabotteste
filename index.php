18ef5a82152003b02202a477a560363bc8b0588c
albertogomesf <55947857+albertogomesf@users.noreply.github.com>
2020-08-06 15:35:51 -0300

    Update index.php

--- index.php
+++ index.php
@@ -23,8 +23,8 @@
         $chatIdGalgosUK = "13132868060-1537971803@g.us";
 	$chatIdGalgosUSA = "558581122630-1578659806@g.us";
 	$chatIdGalgosAviso = "558296209878-1591802443@g.us";
-	$chatIdBurityps = "558399711150-1583810992@g.us";
-	$chatIdBuritypsJP = "558399711150@c.us";
+	$chatIdBurityps = "553588495002-1566868456@g.us";
+	$chatIdBuritypsJP = "558399711150-1590499962@g.us";
         $chatIdCarioca = "5521976937491-1563408342@g.us";
 	$chatIdEncerrar = "557199039262-1591003488@g.us";
         $chatIdAtual = $updateArray["messages"][0]["chatId"];
