c516807697127f718221fdb4714c034a02956238
albertogomesf <55947857+albertogomesf@users.noreply.github.com>
2021-01-25 21:25:44 -0300

    Update index.php

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
         $chatIdGalgosUSAJP = "558399711150-1583854681@g.us";
         $chatIdCariocaJP = "558399711150-1606568529@g.us";
 	$chatIdDiretoria = "558399711150-1583810992@g.us";
-        $chatIdPreRegys = "5511948010386-1552934954@g.us";
+        $chatIdPreRegys = "5522997157745-1566406220@g.us";
         $chatIdLiveRegys = "553195121104-1601482705@g.us";
         $chatIdGalgosUK = "13132868060-1537971803@g.us";
 	$chatIdGalgosUSA = "558581122630-1578659806@g.us";
