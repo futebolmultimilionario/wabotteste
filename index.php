d973c4ad2eb16fe1f11fed7da3d8b17f0312c365
albertogomesf <55947857+albertogomesf@users.noreply.github.com>
2021-02-25 15:10:09 -0300

    Update index.php

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
                 	"Accept: application/json\r\n"
     	)
 	);
-	$url = "https://ee16e49b5a87.ngrok.io/tete/index.php";
+	$url = "https://estruturatexto.herokuapp.com/index.php";
 	$context  = stream_context_create( $options );
 	
 
