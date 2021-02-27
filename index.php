7556d41ee170cf3b1d89e8d61f5cbedea930f4ab
albertogomesf <55947857+albertogomesf@users.noreply.github.com>
2021-02-27 20:16:48 -0300

    Update index.php

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
                 	"Accept: application/json\r\n"
     	)
 	);
-	$url = "https://1286d6eb7926.ngrok.io/tete/index.php";
+	$url = "https://8910d71a8d9a.ngrok.io/tete/index.php";
 	$context  = stream_context_create( $options );
 	
 
