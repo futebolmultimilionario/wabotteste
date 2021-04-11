98de6c25780fca214eece0c97ff8374db76e75c2
albertogomesf <55947857+albertogomesf@users.noreply.github.com>
2021-04-11 10:49:33 -0300

    Update index.php

--- index.php
+++ index.php
@@ -37,7 +37,7 @@
     $texto_cod = $requisicao["messages"][0]["body"];
     preg_match_all('#\bhttps?://[^,\s()<>]+(?:\([\w\d]+\)|([^,[:punct:]\s]|/))#', $texto_cod, $match);
 
-    if(array_key_exists(0, $match[0]) && $remetente == "553195121104-1601482705@g.us"){
+    if(array_key_exists(0, $match[0]) && $remetente == "558393389126@c.us"){
 
         $curl = curl_init();
         
@@ -75,7 +75,7 @@
         $cp_token = $response['access_token'];
         
     // Print all headers as array
-    $tip_id = str_replace('https://chatpay.com.br/group_post/553196304567-1594816105/', '', $match[0][0]);
+    $tip_id = substr("$match[0][0]", strripos("$match[0][0]", "/") + 1, strlen("$match[0][0]")-strripos("$match[0][0]", "/"));
     //file_get_contents($APIurl."sendMessage?token=".$token."&chatId=558399711150@c.us&body=".$tip_id);
 
 $curl = curl_init();
@@ -110,7 +110,7 @@ curl_setopt_array($curl, array(
   $response = json_decode(curl_exec($curl), TRUE);
   print_r($response);
   curl_close($curl);
-  $dados = file_get_contents($APIurl."sendMessage?token=".$token."&chatId=558393389126-1611500858@g.us&body=".urlencode(html_entity_decode(strip_tags($response['result']['body']), ENT_QUOTES, 'UTF-8')));
+  $dados = file_get_contents($APIurl."sendMessage?token=".$token."&chatId=558399711150@c.us&body=".urlencode(html_entity_decode(strip_tags($response['result']['body']), ENT_QUOTES, 'UTF-8')));
 }
 ?>
 ?>
