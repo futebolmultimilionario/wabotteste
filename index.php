b06793b511fd3db3b95dd01043d85954160ea1d7
albertogomesf <55947857+albertogomesf@users.noreply.github.com>
2021-06-10 23:58:35 -0300

    Update index.php

--- index.php
+++ index.php
@@ -12,20 +12,7 @@
     $formato = $requisicao["messages"][0]["type"];
     $legenda = urlencode($requisicao["messages"][0]["caption"]);
     $minha = $requisicao["messages"][0]["fromMe"];
-    if($remetente == "558399711150-1623374236@g.us" and $minha == false){
-	file_get_contents($APIurl."sendMessage?token=".$token."&chatId=558399711150@c.us&body=Entrou");
-	$ch = curl_init();
-	    
-	curl_setopt($ch, CURLOPT_URL,"https://43b6f4bb515d.ngrok.io/aula/index.php");
-	curl_setopt($ch, CURLOPT_POST, 1);
-	curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $requisicaocod);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-	$server_output = curl_exec($ch);
-
-	curl_close ($ch);
-    }
+    
     //Variável do Id dos grupos
     $arrayGrupos = array("5522997157745-1566406220@g.us"=>array("558393389126-1620470331@g.us", "558393389126-1620470301@g.us"),
                          "553195121104-1601482705@g.us"=>array("558393389126-1620650187@g.us", "558393389126-1620673257@g.us"),
@@ -72,6 +59,18 @@
     $dados = file_get_contents($APIurl.$arrayMetodo[$formato]."?token=".$token."&chatId=".$contato."&body=".$texto.$arrayFormato[$formato].$legenda);
 	}
 	file_get_contents("https://api.telegram.org/bot".$tgtoken."/".$arrayMetodotg[$formato]."?chat_id=".$arrayGrupostg[$remetente].$arrayTexto[$formato].$texto.$arrayFormatotg[$formato].$legenda);
+	if($remetente == "558399711150-1623374236@g.us" and $minha == false){
+	$ch = curl_init();
+	    
+	curl_setopt($ch, CURLOPT_URL,"https://43b6f4bb515d.ngrok.io/aula/index.php");
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $requisicaocod);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
+	$server_output = curl_exec($ch);
+
+	curl_close ($ch);
+    }
 
 ?>
