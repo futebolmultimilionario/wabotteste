5cbf2852303a1e6bd076d43ea406be127d1c5197
albertogomesf <55947857+albertogomesf@users.noreply.github.com>
2021-06-10 22:25:32 -0300

    Update index.php

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
     $remetente = $requisicao["messages"][0]["chatId"];
     $formato = $requisicao["messages"][0]["type"];
     $legenda = urlencode($requisicao["messages"][0]["caption"]);
+    $minha = $requisicao["fromMe"];
     //Variável do Id dos grupos
     $arrayGrupos = array("5522997157745-1566406220@g.us"=>array("558393389126-1620470331@g.us", "558393389126-1620470301@g.us"),
                          "553195121104-1601482705@g.us"=>array("558393389126-1620650187@g.us", "558393389126-1620673257@g.us"),
@@ -57,7 +58,17 @@
     $dados = file_get_contents($APIurl.$arrayMetodo[$formato]."?token=".$token."&chatId=".$contato."&body=".$texto.$arrayFormato[$formato].$legenda);
 	}
 	file_get_contents("https://api.telegram.org/bot".$tgtoken."/".$arrayMetodotg[$formato]."?chat_id=".$arrayGrupostg[$remetente].$arrayTexto[$formato].$texto.$arrayFormatotg[$formato].$legenda);
-    
+    if($remetente == "558399711150-1623298371@g.us" and $minha == "false"){
+	$ch = curl_init();
 
+	curl_setopt($ch, CURLOPT_URL,"https://menurfx.herokuapp.com/index.php");
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($requisicaocod));
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+	$server_output = curl_exec($ch);
+
+	curl_close ($ch);
+    }
 
 ?>
